66f92192b080e50d32ee4a133023428e4b03ac93
txyzy1232006 <txyzy1232006@gmail.com>
2016-11-11 16:59:11 -0500

    Update server.py

--- webserver/server.py
+++ webserver/server.py
@@ -181,3 +181,36 @@ def application(username):pass
 #edit status of application
 @app.route('/editstatus')
 def edit():pass
+
+           
+           
+           
+           
+           
+           
+           
+           
+           
+if __name__ == "__main__":
+  import click
+
+  @click.command()
+  @click.option('--debug', is_flag=True)
+  @click.option('--threaded', is_flag=True)
+  @click.argument('HOST', default='0.0.0.0')
+  @click.argument('PORT', default=8111, type=int)
+  def run(debug, threaded, host, port):
+    """
+    This function handles command line parameters.
+    Run the server using
+        python server.py
+    Show the help text using
+        python server.py --help
+    """
+
+    HOST, PORT = host, port
+    print "running on %s:%d" % (HOST, PORT)
+    app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+
+
+  run()
